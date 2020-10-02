930533bf0f5dc9766d69e25d70d3b45e39abecfd
nguyenanhung <info@nguyenanhung.com>
2020-10-02 23:38:57 +0700

    Release version 2.0.0

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -17,8 +17,8 @@ namespace nguyenanhung\SEO;
  */
 interface ProjectInterface
 {
-    const VERSION             = '1.0.5';
-    const LAST_MODIFIED       = '2019-10-04';
+    const VERSION             = '2.0.0';
+    const LAST_MODIFIED       = '2019-10-02';
     const AUTHOR_NAME         = 'Hung Nguyen';
     const AUTHOR_EMAIL        = 'dev@nguyenanhung.com';
     const PROJECT_NAME        = 'Search Engine Optimization Manager';
