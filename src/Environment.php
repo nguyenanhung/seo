678cc3ca69b22f40e823bf46c704d701780b98ce
nguyenanhung <info@nguyenanhung.com>
2021-09-16 02:00:04 +0700

    Release version 2.0.2

--- src/Environment.php
+++ src/Environment.php
@@ -17,8 +17,8 @@ namespace nguyenanhung\SEO;
  */
 interface Environment
 {
-    const VERSION       = '2.0.1';
-    const LAST_MODIFIED = '2021-09-15';
+    const VERSION       = '2.0.2';
+    const LAST_MODIFIED = '2021-09-16';
     const AUTHOR_NAME   = 'Hung Nguyen';
     const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     const PROJECT_NAME  = 'Search Engine Optimization Manager';
