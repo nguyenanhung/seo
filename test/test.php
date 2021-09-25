4cf5d9f6ab69d5ebe339de81a16c5520fa99e898
nguyenanhung <info@nguyenanhung.com>
2021-09-25 11:52:32 +0700

    Update TestCase

--- test/test.php
+++ test/test.php
@@ -7,3 +7,28 @@
  * Date: 09/24/2021
  * Time: 05:22
  */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use nguyenanhung\SEO\SeoUrl;
+
+$config = array(
+    'salt'          => 'w40):pc6cwS{mn9I_O=B$2Cr;=YXA#',
+    'minHashLength' => 8,
+    'alphabet'      => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+);
+
+$seo = new SeoUrl();
+$seo->setHashIds($config);
+
+$id    = 20210925;
+$title = 'Nguyễn An Hưng';
+
+echo "title: " . $title . PHP_EOL;
+echo "Slugify: " . $seo->slugify($title) . PHP_EOL;
+echo "search_slugify: " . $seo->search_slugify($title) . PHP_EOL;
+echo "strToEn: " . $seo->strToEn($title) . PHP_EOL;
+echo "randomId: " . $seo->randomId() . PHP_EOL;
+
+echo "fromId: " . $id . PHP_EOL;
+echo "encodeId: " . $seo->encodeId($id) . PHP_EOL;
+echo "decodeId: " . $seo->decodeId($seo->encodeId($id)) . PHP_EOL;
