72bade86a1ada398ce4f8507074ac6893f34fbbd
nguyenanhung <info@nguyenanhung.com>
2022-07-20 02:26:17 +0700

    Release v3.0.7.1

--- src/Environment.php
+++ src/Environment.php
@@ -17,7 +17,7 @@ namespace nguyenanhung\SEO;
  */
 interface Environment
 {
-    const VERSION       = '3.0.7';
+    const VERSION       = '3.0.7.1';
     const LAST_MODIFIED = '2022-07-20';
     const AUTHOR_NAME   = 'Hung Nguyen';
     const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
