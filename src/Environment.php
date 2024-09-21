32b66d9963f3b70a5a8ea90afd697e363c4f59d3
Hung Nguyen <dev@nguyenanhung.com>
2024-09-22 02:34:47 +0700

    Update Environment.php

--- src/Environment.php
+++ src/Environment.php
@@ -17,8 +17,8 @@ namespace nguyenanhung\SEO;
  */
 interface Environment
 {
-    const VERSION = '2.1.6';
-    const LAST_MODIFIED = '2023-10-19';
+    const VERSION = '2.1.7';
+    const LAST_MODIFIED = '2024-09-22';
     const AUTHOR_NAME = 'Hung Nguyen';
     const AUTHOR_EMAIL = 'dev@nguyenanhung.com';
     const PROJECT_NAME = 'Search Engine Optimization Manager';
